6e7d1d72d643667b8a28ff4b6713129c98923c09
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-05-03 14:27:00 +0800

    Added webhookSecret in modules

--- medusa-config.ts
+++ medusa-config.ts
@@ -33,6 +33,7 @@ module.exports = defineConfig({
             id: "my-payment",
             options: {
               apiKey: process.env.STRIPE_API_KEY,
+              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
             },
           },
         ],
