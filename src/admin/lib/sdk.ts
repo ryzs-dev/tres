2718e8a5b0bcdd7b5b04ef801084917ab1c01795
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-06-04 22:55:33 +0800

    fix: configure base url

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk";
 
 export const sdk = new Medusa({
-  baseUrl: "https://admin.tres.my",
+  baseUrl: process.env.ADMIN_CORS || "https://admin.tres.my",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
