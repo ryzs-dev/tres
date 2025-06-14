3c45099e4ff316c9afe6b79a78531f8f4efb01c2
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-06-15 01:51:08 +0800

    fix : medusa config

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,7 +4,16 @@ import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+  },
   projectConfig: {
+    redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | "shared"
+      | "worker"
+      | "server",
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
@@ -48,6 +57,26 @@ module.exports = defineConfig({
       },
     },
     {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+    {
       resolve: "@medusajs/medusa/file",
       options: {
         providers: [
@@ -69,9 +98,6 @@ module.exports = defineConfig({
           },
         ],
       },
-    },
-    {
-      resolve: "./src/modules/bundled-product",
     },
   ],
 });
