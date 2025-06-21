dab96cdde96685445a1a7e4ec735cee8ad88eef5
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-06-22 02:53:58 +0800

    fix sdk https issue

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk";
 
 export const sdk = new Medusa({
-  baseUrl: "http://admin.tres.my",
+  baseUrl: "https://admin.tres.my",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
