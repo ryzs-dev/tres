b89bc0ba0b93bef5b3323664ed8f23b6fb93806b
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2025-10-13 20:14:51 +0800

    feat(draft-order): Added Draft Order Plugin

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,6 +4,12 @@ import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+  plugins: [
+    {
+      resolve: "@medusajs/draft-order",
+      options: {},
+    },
+  ],
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
     backendUrl: process.env.MEDUSA_BACKEND_URL,
@@ -16,9 +22,9 @@ module.exports = defineConfig({
       | "server",
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
+      storeCors: "http://localhost:8000",
       adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      authCors: "http://localhost:8000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
